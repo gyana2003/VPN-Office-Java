a7dc89933b7eb8f3ea6ef886aa23589fa72bbab6
Gyana Ranjan Jena <131476067+gyana2003@users.noreply.github.com>
2025-05-31 23:33:42 +0530

    Create MarketingServlet.java

---
+++ MarketingServlet.java
@@ -0,0 +1,15 @@
+package com.vpnproject;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class MarketingServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h2>Marketing Module</h2>");
+        out.println("<p>Securely share business information.</p>");
+    }
+}
